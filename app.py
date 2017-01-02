45d3aadfe68794e3a1322e7c78d912995cb7c33b
Gaeun Kim <gaeunkim@ohs.stanford.edu>
2017-01-02 21:22:36 +0900

    test

--- app.py
+++ app.py
@@ -16,11 +16,12 @@ def hello_world():
 
 @webhook.hook("pull_request")
 def on_pull_request(response):
-	data = json.loads(response)
-	if data.branch == 'gh-pages' and (data.action == 'opened' or data.action == 'reopened'):
+	print(response)
+	data = json.loads(response)	
+#if data.branch == 'gh-pages' and (data.action == 'opened' or data.action == 'reopened'):
+	if data.action == 'opened' or data.action == 'reopened':
 		pull_request_id = data.number
 		g.get_repo(data.repository.id).get_issue(pull_request_id).create_comment('Hello')
-	print(data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
